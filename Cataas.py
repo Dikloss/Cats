ccbe3ca06537ab1888f73b10d2f063721a30202d
Dikloss <diklos00@gmail.com>
2024-11-14 14:01:47 +0300

    Добавили кнопку для загрузки нового изрображения

--- Cataas.py
+++ Cataas.py
@@ -4,10 +4,11 @@ import requests
 from io import BytesIO
 
 from bottle import response
+from pygame.display import update
 from samtranslator.model.packagetype import IMAGE
 
 
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -18,6 +19,14 @@ def load_image():
         print(f"Произошла ошибка {e}")
         return None
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
+
 window = Tk()
 window.title("Cats")
 window.geometry("600x480")
@@ -25,11 +34,12 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
-url = "https://cataas.com/cat"
-img = load_image(url)
+update_button = Button(text="", command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img)
+url = "https://cataas.com/cat"
+
+set_image()
 
 window.mainloop()
 
