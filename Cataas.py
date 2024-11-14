dd919f136981c7d7b8be441f6574a81ebf2e8bca
Dikloss <diklos00@gmail.com>
2024-11-14 13:43:04 +0300

    Сщздано окно image

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,23 @@
-from tkinter import  *
+from tkinter import *
+from PIL import Image,ImageTk
+import requests
+from io import BytesIO
+
+from pygame.examples.cursors import image
+
+window = Tk()
+window.title("Cats")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = "https://cataas.com/cat"
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+
+window.mainloop()
+
+
